66bfcfabb5928813cf8d490cf39fd5bd3e2e83ab
Jeremy C. Andrus <jeremya@cs.columbia.edu>
2011-10-31 10:39:04 -0400

    check for clam binary before doing regression testing

--- clam/tester.sh
+++ clam/tester.sh
@@ -1,4 +1,25 @@
 #!/bin/bash
+#
+# Regression tester for CLAM compiler
+#
+# PLT Fall 2011
+# Jeremy Andrus <jeremya@cs.columbia.edu>
+# Robert Martin <rdm2128@columbia.edu>
+# Kevin Sun <kfs2110@columbia.edu>
+# Yongxu Zhang <yz2419@columbia.edu>
+#
+
+# Make sure we have a clam binary to work with ;-)
+CLAM="clam"
+if [ ! -x "$CLAM" ]; then
+	CLAM=`which clam`
+fi
+if [ -z "$CLAM" -o ! -x "$CLAM" ]; then
+	echo "Couldn't find the 'clam' binary."
+	echo "Aborting regression test suite."
+	echo ""
+	exit 1
+fi
 
 function err() {
 	echo -e "\033[0;31m[E] $1\033[0m"
